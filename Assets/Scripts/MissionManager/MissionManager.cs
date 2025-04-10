f876edacdaed64a3577a62a5a7965a8ca3f478e8
keovanpay <100199799+keovanpay@users.noreply.github.com>
2025-04-10 21:25:01 +0700

    Initial commit

---
+++ Assets/Scripts/MissionManager/MissionManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class MissionManager : MonoBehaviour
+{
+    public static MissionManager instance;
+
+    public Mission currentMission;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        Invoke(nameof(StartMission), 2);
+    }
+
+    private void Update()
+    {
+        currentMission?.UpdateMission();
+    }
+
+    private void StartMission() => currentMission.StartMission();
+
+    public bool MissionCompleted() => currentMission.MissionCompleted();
+}
---
+++ Assets/Scripts/MissionManager/Mission_Timer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Timer Mission", menuName = "Mission/Timer mission")]
+
+public class Mission_Timer : Mission
+{
+    public float time;
+
+    private float currentTime;
+
+    public override void StartMission()
+    {
+        currentTime = time;
+    }
+
+    public override void UpdateMission()
+    {
+        currentTime -= Time.deltaTime;
+
+        if (currentTime < 0)
+        {
+            Debug.Log("GAME OVER");
+        }
+
+        string timeText = System.TimeSpan.FromSeconds(currentTime).ToString("mm':'ss");
+
+        Debug.Log(timeText);
+    }
+
+    public override bool MissionCompleted()
+    {
+        return currentTime > 0;
+    }
+}
